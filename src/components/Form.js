e2421cb4a908505a6f20c6d52ccc742f73819af4
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-08-24 19:11:51 +0200

    delete unused files

---
+++ src/components/Form.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import List from "./List";
+
+class Form extends Component {
+  state = {
+    kind: "",
+    product: "",
+    list: [],
+  };
+
+  // functions to add data from select and input to Form state
+  handleChange = (e) => {
+    this.setState({
+      kind: e.target.value,
+    });
+  };
+
+  handleChange2 = (e) => {
+    this.setState({
+      product: e.target.value,
+    });
+  };
+
+  addProduct = (e) => {
+    e.preventDefault();
+    const toBuy_List = this.state.list;
+    toBuy_List.push(this.state.product);
+    this.setState({
+      list: toBuy_List,
+    });
+    document.querySelector("input").value = "";
+  };
+
+  render() {
+    return (
+      <>
+        <form action="">
+          <label>What list do you want to write?</label>
+          <select name="kind" id="kind-select" onChange={this.handleChange}>
+            <option value="fruits">fruits</option>
+            <option value="vegetables">vegetables</option>
+            <option value="cereals">cereals</option>
+            <option value="other">other</option>
+          </select>
+          <label>What do you need to buy?</label>
+          <input
+            type="text"
+            placeholder="- - - - - - - - - - - - - - -"
+            onChange={this.handleChange2}
+          />
+          <button type="submit" onClick={this.addProduct}>
+            Add
+          </button>
+        </form>
+        {/* add state infos to child component */}
+        <List newlist={this.state} />
+      </>
+    );
+  }
+}
+
+export default Form;
